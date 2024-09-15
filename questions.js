bded05d968033853c6fde49385a959ef74b7b157
Maddy6914 <madmadhan661@gmail.com>
2024-09-15 22:25:25 +0530

    website done for bigscreen by maddy

---
+++ questions.js
@@ -0,0 +1,65 @@
+let questions =[
+  {
+    numb:1,
+    question:"What Does HTML Stands For?",
+    answer:"C. Hyper Text Markup Language",
+    options:[
+        "A. Hyper Type Multi Language",
+        "B. Hyper Text Multiple Language",
+        "C. Hyper Text Markup Language",
+        "D. Home Text Multi Language",
+    ]
+  },
+
+  {
+    numb:2,
+    question:"What does CSS stand for?",
+    answer:"A. Cascading Style Sheet",
+    options:[
+        "A. Cascading Style Sheet",
+        "B. Cute Style Sheet",
+        "C. Computer Style Sheet",
+        "D. Cast Style Sheet"
+
+    ]
+  },
+
+  {
+    numb:3,
+    question:"What Does PHP Stand For?",
+    answer:"A. Hypertext Preprocessor",
+    options:[
+        "A. Hypertext Preprocessor",
+        "B. Hometext Programming",
+        "C. Hypertext Programming",
+        "D. Programming Preprocessor"
+
+    ]
+  },
+
+  {
+    numb:4,
+    question:"What Does SQL Stand For?",
+    answer:"D. Structured Query Language",
+    options:[
+        "A. Strength Query Language",
+        "B. Stylesheet Query Language",
+        "C. Science Question Language",
+        "D. Structured Query Language"
+
+    ]
+  },
+
+  {
+    numb:5,
+    question:"What Does XML Means?",
+    answer:"D. Extensible Markup Language",
+    options:[
+        "A. Excellent Multiple Language",
+        "B. Explore Multiple Language",
+        "C. Extra Markup Language",
+        "D. Extensible Markup Language"
+    ]
+  },
+]
+
